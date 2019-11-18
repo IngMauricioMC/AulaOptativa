1cf0b11ee9d27185382e857ebe703930c97d430d
LUIS MAURICIO MOSQUERA CIFUENTES <54857927+IngMauricioMC@users.noreply.github.com>
2019-11-18 19:28:41 -0300

    Add files via upload

---
+++ paginas/formulario.php
@@ -0,0 +1,50 @@
+<?php
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<link rel="stylesheet" type="text/css" href="style/estilo-grid.css">
+ 	<title>MyWebSite</title> <!-- Projeto WebService 2019 -->
+ 	<meta charset="utf-8">
+ 	<link rel="stylesheet" type="text/css" href="../style/estilo-grid-768.css" media="screen and (max-width:768px">
+ 	<link rel="stylesheet" type="text/css" href="../style/estilo-grid-480.css" media="screen and (max-width:480px">
+ </head>
+ <body>
+ 	<div id="container">
+ 		<div id="header">
+ 			<h1>Casa de Papel</h1><!-- Título do site -->
+ 		</div>
+ 		<div id="menu">
+ 			<ul>
+ 				<li><a href="">Home</a></li>
+ 				<Li>Sobre</Li>
+ 				<!-- <Li>Sobre</Li> -->
+ 				<!-- <Li>Sobre</Li>
+ 				<Li>Sobre</Li>
+ 				<Li>Sobre</Li>
+ 				<Li>Sobre</Li>
+ 				<Li>Sobre</Li>
+ 				<Li>Sobre</Li> -->
+ 			</ul>
+ 		</div>
+ 				<!-- <div id="menu-top"></div>
+ 				<div id="menu-left"></div> -->
+ 		<div id="main">
+ 			<div id="content">
+ 				<!-- <h1>Título1</h1>
+      				<p>Texto1</p> -->
+ 			</div>
+ 			<div id="menu-lateral">
+ 				<Li>Home</Li>
+ 				<Li>Sob</Li>
+ 				<Li>Sobre</Li>
+ 			</div>
+ 		</div>
+ 		<div id="footer">
+ 			<p>--- Todos os direitos resevados ---</p>
+ 		</div>
+ 	</div>
+ </body>
+ </html>
